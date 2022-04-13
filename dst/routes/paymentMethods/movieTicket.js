9964923e45b0e62502771e037166df23f7cafddf
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2022-04-13 13:39:26 +0900

    update @cinerino/sdk

--- dst/routes/paymentMethods/movieTicket.js
+++ dst/routes/paymentMethods/movieTicket.js
@@ -13,88 +13,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * 決済カードルーター
  */
 const sdk_1 = require("@cinerino/sdk");
-// import * as createDebug from 'debug';
 const express = require("express");
 const http_status_1 = require("http-status");
-// const debug = createDebug('cinerino-console:routes');
 const movieTicketPaymentMethodRouter = express.Router();
 /**
  * 決済カード認証
  */
 movieTicketPaymentMethodRouter.get('/check', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b;
     try {
         const payService = new sdk_1.chevre.service.assetTransaction.Pay({
             endpoint: process.env.API_ENDPOINT,
             auth: req.user.authClient,
             project: { id: req.project.id }
         });
-        const searchConditions = {
-            seller: { id: req.query.seller },
-            identifier: req.query.identifier,
-            accessCode: req.query.accessCode,
-            serviceOutput: {
-                reservationFor: {
-                    id: (_b = (_a = req.query.serviceOutput) === null || _a === void 0 ? void 0 : _a.reservationFor) === null || _b === void 0 ? void 0 : _b.id
-                }
-            }
-        };
+        const productService = new sdk_1.chevre.service.Product({
+            endpoint: process.env.API_ENDPOINT,
+            auth: req.user.authClient,
+            project: { id: req.project.id }
+        });
         if (req.query.format === 'datatable') {
-            const paymentMethodType = req.query.paymentMethodType;
-            const checkAction = yield payService.check({
-                project: { id: req.project.id, typeOf: sdk_1.chevre.factory.organizationType.Project },
-                typeOf: sdk_1.chevre.factory.actionType.CheckAction,
-                agent: {
-                    typeOf: sdk_1.chevre.factory.personType.Person,
-                    id: req.user.profile.sub,
-                    name: `${req.user.profile.given_name} ${req.user.profile.family_name}`
-                },
-                object: [{
-                        typeOf: sdk_1.chevre.factory.service.paymentService.PaymentServiceType.MovieTicket,
-                        id: '',
-                        paymentMethod: {
-                            typeOf: paymentMethodType,
-                            additionalProperty: [],
-                            name: paymentMethodType,
-                            paymentMethodId: '' // 使用されないので空でよし
-                        },
-                        movieTickets: [{
-                                project: { typeOf: req.project.typeOf, id: req.project.id },
-                                typeOf: paymentMethodType,
-                                identifier: searchConditions.identifier,
-                                accessCode: searchConditions.accessCode,
-                                serviceType: '',
-                                serviceOutput: {
-                                    reservationFor: {
-                                        typeOf: sdk_1.chevre.factory.eventType.ScreeningEvent,
-                                        id: searchConditions.serviceOutput.reservationFor.id
-                                    },
-                                    reservedTicket: {
-                                        ticketedSeat: {
-                                            typeOf: sdk_1.chevre.factory.placeType.Seat,
-                                            seatNumber: '',
-                                            seatRow: '',
-                                            seatSection: ''
-                                        }
-                                    }
-                                }
-                            }],
-                        seller: {
-                            typeOf: sdk_1.chevre.factory.organizationType.Corporation,
-                            id: String(searchConditions.seller.id)
-                        }
-                    }]
-            });
+            const actionAttributes = yield creatCheckMovieTicketActionAttributes({ req })({ product: productService });
+            const checkAction = yield payService.check(actionAttributes);
             const result = checkAction.result;
             if (result === undefined) {
                 throw new Error('checkAction.result undefined');
             }
-            // res.json({
-            //     draw: req.body.draw,
-            //     recordsTotal: result.movieTickets.length,
-            //     recordsFiltered: result.movieTickets.length,
-            //     data: result.movieTickets
-            // });
             res.json({
                 success: true,
                 count: result.movieTickets.length,
@@ -102,10 +45,7 @@ movieTicketPaymentMethodRouter.get('/check', (req, res, next) => __awaiter(void 
             });
         }
         else {
-            res.render('paymentMethods/movieTicket/check', {
-            // searchConditions: searchConditions,
-            // sellers: sellers
-            });
+            res.render('paymentMethods/movieTicket/check', {});
         }
     }
     catch (error) {
@@ -118,4 +58,65 @@ movieTicketPaymentMethodRouter.get('/check', (req, res, next) => __awaiter(void 
         }
     }
 }));
+function creatCheckMovieTicketActionAttributes(params) {
+    return (repos) => __awaiter(this, void 0, void 0, function* () {
+        var _a, _b, _c;
+        const req = params.req;
+        const paymentServiceId = req.query.paymentServiceId;
+        const searchPaymentServicesResult = yield repos.product.search({
+            limit: 1,
+            page: 1,
+            id: { $eq: paymentServiceId }
+        });
+        const paymentService = searchPaymentServicesResult.data.shift();
+        if (paymentService === undefined) {
+            throw new Error('決済サービスが見つかりません');
+        }
+        const paymentMethodType = String((_a = paymentService.serviceType) === null || _a === void 0 ? void 0 : _a.codeValue);
+        return {
+            project: { id: req.project.id, typeOf: sdk_1.chevre.factory.organizationType.Project },
+            typeOf: sdk_1.chevre.factory.actionType.CheckAction,
+            agent: {
+                typeOf: sdk_1.chevre.factory.personType.Person,
+                id: req.user.profile.sub,
+                name: `${req.user.profile.given_name} ${req.user.profile.family_name}`
+            },
+            object: [{
+                    typeOf: sdk_1.chevre.factory.service.paymentService.PaymentServiceType.MovieTicket,
+                    id: paymentServiceId,
+                    paymentMethod: {
+                        typeOf: paymentMethodType,
+                        additionalProperty: [],
+                        name: paymentMethodType,
+                        paymentMethodId: '' // 使用されないので空でよし
+                    },
+                    movieTickets: [{
+                            project: { typeOf: req.project.typeOf, id: req.project.id },
+                            typeOf: paymentMethodType,
+                            identifier: req.query.identifier,
+                            accessCode: req.query.accessCode,
+                            serviceType: '',
+                            serviceOutput: {
+                                reservationFor: {
+                                    typeOf: sdk_1.chevre.factory.eventType.ScreeningEvent,
+                                    id: (_c = (_b = req.query.serviceOutput) === null || _b === void 0 ? void 0 : _b.reservationFor) === null || _c === void 0 ? void 0 : _c.id
+                                },
+                                reservedTicket: {
+                                    ticketedSeat: {
+                                        typeOf: sdk_1.chevre.factory.placeType.Seat,
+                                        seatNumber: '',
+                                        seatRow: '',
+                                        seatSection: ''
+                                    }
+                                }
+                            }
+                        }],
+                    seller: {
+                        typeOf: sdk_1.chevre.factory.organizationType.Corporation,
+                        id: String(req.query.seller)
+                    }
+                }]
+        };
+    });
+}
 exports.default = movieTicketPaymentMethodRouter;
